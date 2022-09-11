94a510b081181d85ff10191809702c69b6e696c5
Rosario Daurat <rosariodaurat@gmail.com>
2022-09-11 13:26:56 -0300

    cambios gameManager

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
     public GameObject suelo;
+    public GameObject jugador;
+    public RawImage corteDeLuz;
 
     private int limiteDeFps = 30;
 
