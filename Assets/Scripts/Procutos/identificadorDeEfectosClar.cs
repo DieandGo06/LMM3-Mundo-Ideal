fcd866ae0d0bb3f84f016a31d0d1d6011c5f2840
clarafallesen <clarafallesen44@gmail.com>
2022-09-12 00:37:55 -0300

    intento paredesx2

--- Assets/Scripts/Procutos/identificadorDeEfectosClar.cs
+++ Assets/Scripts/Procutos/identificadorDeEfectosClar.cs
@@ -8,11 +8,16 @@ public class identificadorDeEfectosClar : MonoBehaviour
     public bool esSaludable;
     public bool seEjecutaUnaVez;
     public string nombreDeProducto;
+
     private float aparecer;
-    private float aparecer1;
-    
-    
-    
+
+    //Para el temporizador de las paredes
+    public float tiempoMax;
+    float tiempoTranscurrido;
+    bool apareciendo;
+    [SerializeField] float speed;
+
+
 
     // Start is called before the first frame update
     void Start()
@@ -41,8 +46,8 @@ public class identificadorDeEfectosClar : MonoBehaviour
                     if (nombreDeProducto == "papitas")
                     {
                         Tareas.NuevaConDuracion(0, 4, AparecerParedSucia);
-                        
 
+                        apareciendo = true;
 
                         //ACA LE DETERMINO EN QUE SEGUNDO LUEGO DE TOMAR EL EFECTO TIENE QUE PRENDERSE Y APAGARSE LA LOooz!!
                         Tareas.Nueva(0.5f, GlitchOff);
@@ -68,10 +73,40 @@ public class identificadorDeEfectosClar : MonoBehaviour
 
                 }
             }
+            if (apareciendo)
+            {
+                AparecerParedSucia();
+                tiempoTranscurrido += Time.deltaTime;
+            }
+
+            if (tiempoTranscurrido > tiempoMax)
+            {
+                Detener();
+            }
         }
     }
+    //Temporizador y logica de las paredes ------------------------------------------
+   
 
-    void GlitchOff () //ESTA ES LA PANTALLA EN NEGRO
+    void Detener()
+    {
+        apareciendo = false;
+        //GetComponent<TrailRenderer>().emitting = false;
+    }
+
+   
+    void AparecerParedSucia()
+    {
+        
+
+        //aparecer += Time.deltaTime / 4;
+        
+        GameObject coso = GameManager.instance.paredes.transform.GetChild(0).gameObject;
+        coso.GetComponent<Renderer>().material.color = new Color(1, 1, 1, tiempoTranscurrido/5);
+        
+    }
+
+    void GlitchOff () //ESTA ES LA PANTALLA EN NEGRO------------------------------------------------
     {
         
         float alpha = Random.Range(0.003f,0.008f); 
@@ -85,13 +120,8 @@ public class identificadorDeEfectosClar : MonoBehaviour
 
     }
 
-    void AparecerParedSucia()
-    {
-        aparecer += Time.deltaTime / 4;
-        //float limitador = Mathf.Clamp(aparecer, 0, 1);
-        GameObject coso = GameManager.instance.paredes.transform.GetChild(0).gameObject;
-        coso.GetComponent<Renderer>().material.color =new Color(1, 1, 1, aparecer);
-        Debug.Log("holi");
-    }
-  
+    
+
+   
+
 }
